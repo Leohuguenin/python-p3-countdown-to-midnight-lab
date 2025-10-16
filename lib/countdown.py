a7c97d803694bdc079383d3262df1551a40ea3ff
Leohuguenin <leonardohuguenin@live.com>
2025-10-16 11:53:02 +1100

    Add countdown functions with and without sleep
    Implemented countdown and countdown_with_sleep functions in countdown.py. countdown prints seconds remaining, while countdown_with_sleep adds a 1-second delay between prints.

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
-# your code goes here!
+def countdown(x):
+    while x > 0:
+        print(f"{x} SECOND(S)!")
+        x -= 1
+    print("HAPPY NEW YEAR!")
+
+import time
+
+def countdown_with_sleep(x):
+    while x > 0:
+        print(f"{x} SECOND(S)!")
+        x -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+
